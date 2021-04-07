00bf5b98daec738e85809fceb23bb705b92d472d
Tim Seip <tim.seip@capgemini.com>
2021-04-07 13:25:52 +0200

    Basic setup + explanation of how to host

--- api.py
+++ api.py
@@ -3,6 +3,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# because the app is accessed through Expo/React Native you have to host the application on your public network
+# the localhost does NOT work. 
+# run: "flask run --host=192.168.178.11" <- Please check what your local device's IP is, this depends on your situation
+# 'ipconfig' on Windows or `ip address`/`ifconfig` on Linux
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
